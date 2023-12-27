8ccd0234ed2fd1704567f0e036db54422442ae97
inyujeongsang <injeong940202@gmail.com>
2023-12-27 16:56:10 +0900

    익재코드_추가

---
+++ src/kakao/project/student/Student.java
@@ -0,0 +1,63 @@
+package kakao.project.student;
+
+class Student {
+
+    private int ID;
+    private String name;
+    private String Sex;
+    private int PresentTableNumber;
+
+    Student() {
+        ID = 0;
+        name = null;
+        Sex = null;
+        PresentTableNumber = 0;
+    }
+
+    Student(int nID, String nName, String nSex) {
+        ID = nID;
+        name = nName;
+        Sex = nSex;
+    }
+
+    Student(int nID, String nName, String nSex, int nPresentTableNumber) {
+        ID = nID;
+        name = nName;
+        Sex = nSex;
+        PresentTableNumber = nPresentTableNumber;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int iD) {
+        ID = iD;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return Sex;
+    }
+
+    public void setSex(String sex) {
+        this.Sex = sex;
+    }
+
+    public int getPresnetTableNumber() {
+        return PresentTableNumber;
+    }
+
+    public void setPresentTableNumber(int PresentTableNumber) {
+        this.PresentTableNumber = PresentTableNumber;
+    }
+
+}
+
