148d7ab6e312ac39603e5cd47064bb227bb53068
inyujeongsang <injeong940202@gmail.com>
2023-12-29 01:51:55 +0900

    Arrange 멤버변수, 생성자, getter/setter

--- src/kakao/project/arrange/Arrange.java
+++ src/kakao/project/arrange/Arrange.java
@@ -1,5 +1,41 @@
 package kakao.project.arrange;
 
-public class Arrange {
+import kakao.project.student.Student;
 
+import java.awt.*;
+
+public class Arrange {
+    private int seatNum; //자리 번호
+    private Student student; //자리에 배정된 학생, null일 수 있음.
+    private boolean isDesignated; //지정석 여부
+
+    //생성자
+    public Arrange(int seatNum){
+        this.seatNum = seatNum;
+        this.student = null;
+    }
+
+
+    //getter
+    public int getSeatNum(){
+        return seatNum;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public boolean isDesignated() {
+        return isDesignated;
+    }
+
+    // setter
+    public void setDesignated(boolean isDesignated) {
+        this.isDesignated = isDesignated;
+    }
+
+    //지정석 메서드
+    public void assignStudent(Student student) {
+        this.student = student;
+    }
 }
